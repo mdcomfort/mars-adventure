ea39f52f1854f0824500a5f2b1062bdeafa2f2f3
Maya Comfort <mdcomfort@gmail.com>
2021-12-30 18:50:14 -0500

    added new questions

--- MarsAdventure.java
+++ MarsAdventure.java
@@ -16,6 +16,10 @@ class MarsAdventure {
 		String petName = input.nextLine();
 		System.out.println("Roger that, marking down pet " + petName + ".");
 
+		System.out.println("And what kind of animal is that?");
+		String petType = input.nextLine();
+		System.out.println("Ah very well, we look forward to meeting " + petName + " the " + petType + ".");
+
 		System.out.println("What day will you be leaving on your journey?");
 		String departDate = input.nextLine();
 		System.out.println("I suppose we can make " + departDate + " work for you.");
@@ -23,6 +27,22 @@ class MarsAdventure {
 		System.out.println("It's not certain how long we will be gone... However, how long would you like to stay?");
 		String tripLength = input.nextLine();
 		System.out.println("Hm, " + tripLength + ". We shall do our best.");
+
+		while (true) {
+			System.out.println("How many suitcases will you be bringing?");
+			int numSuitcases = input.nextInt();
+			if (numSuitcases > 2) {
+				System.out.println("Sorry, that's too many. 2 or fewer please.");
+			}
+			else if (numSuitcases == 2) {
+				System.out.println("Perfect.");
+				break;
+			}
+			else {
+				System.out.println("Wow, you must have great packing skills!");
+				break;
+			}
+		}
 	}
 }
 	
